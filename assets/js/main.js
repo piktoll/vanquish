38c928a2625f45c296074b216b1a4aa99b3416ba
piktoll <szrenka.viktor.daniel@gmail.com>
2023-02-25 15:36:16 +0100

    Canvas changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,41 @@
 //import * as game from './game';
 
 window.onload = function () {
+
+    class Player {
+        constructor(position) {
+            this.position = position;
+        }
+
+        draw() {
+            gameContext.fillStyle = 'red';
+            gameContext.fillRect(this.position.x, this.position.y, 100, 100);
+        }
+
+        update() {
+            this.draw();
+            this.position.y++;
+        }
+    }
+
+    const gameCanvas = document.querySelector("#canvas");
+    const gameContext = gameCanvas.getContext("2d");
+    gameCanvas.width = window.innerWidth;
+    gameCanvas.height = window.innerHeight;
+
     const game = {
         title: 'Vanquish',
-        startup: function () {
-            const gameCanvas = document.querySelector("#canvas");
-            const gameContext = gameCanvas.getContext("2d");
 
-            gameCanvas.width = window.innerWidth;
-            gameCanvas.height = window.innerHeight;
-
-            gameContext.beginPath();
-            gameContext.rect(0, 0, gameCanvas.width, gameCanvas.height);
-            gameContext.fillStyle = "red";
-            gameContext.fill();
-        },
     };
 
-    game.startup();
+    const player = new Player({ x: 0, y: 0, });
+
+    function animate() {
+        window.requestAnimationFrame(animate);
+        gameContext.fillStyle = 'darkslategrey';
+        gameContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+        player.update();
+    }
+
+    animate();
 }
